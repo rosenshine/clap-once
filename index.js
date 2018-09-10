0b77cede08e79496dbf7fbd1aefe65be23b56abe
brownielox <mrsmpython@gmail.com>
2018-09-10 16:07:50 -0400

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+'use strict'; 
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+const app = express(); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+const server = app.listen(3000, () => { console.log('Express server   listening on port %d in %s mode', server.address().port,   app.settings.env);});
+
+app.post('/', (req, res) => { 
+    let text = req.body.text;   
+  if(!'yes') { // not a response
+    res.send('I didn\'t get that! Are you going to trivia, citizen?');   return; 
+  }
+
+  let data = { 
+    response_type: 'in_channel', // public to the channel 
+    text: 'Great!', 
+    attachments:[ { 
+      image_url: 'https://gph.is/2adpNTu' 
+    } ]}; 
+  res.json(data);
 });
